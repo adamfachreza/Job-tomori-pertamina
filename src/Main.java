a13f3eb6845e4ec4524f5a73282574a952793f01
adamfachreza <adamfachreza15@gmail.com>
2023-03-31 21:50:00 +0700

    [FIX BUG]

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         for (int i = 1; i <= 3; i++) {
-            for (int j = 1; j <= 5; j++)
-            {
-                System.out.println(j);
-                if (j == 5)
-                {
+            for (int j = 1; j <= 5; j++) {
+                System.out.print(j);
+                if (j == 5) {
                     System.out.println();
-                    for (int k = 1; k <= 1; k++)
-                    {
-                        for (int l = 5; l >= 1; l--)
-                        {
-                            System.out.println(l);
-                            if (l == 1)
-                            {
+                    for (int k = 1; k <= 1; k++) {
+                        for (int l = 5; l >= 1; l--) {
+                            System.out.print(l);
+                            if (l == 1) {
                                 System.out.println();
                             }
                         }
